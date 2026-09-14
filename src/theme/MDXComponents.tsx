475196bed8d48334579af6201adf8de7e1d5c221
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-09-14 12:53:10 -0300

    Add LabTable, LabTeamMembers, LabSubmit, LabFromTemplate to MDXComponents

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 // Importa o seu componente customizado
 import LabStructure from '@site/src/components/LabStructure';
+import LabTable from "@site/src/components/LabTable";
+import LabTeamMembers from "@site/src/components/LabTeamMembers";
+import LabSubmit from "@site/src/components/LabSubmit";
+import LabFromTemplate from "@site/src/components/LabFromTemplate";
 
 export default {
     // Mantém os componentes padrão do MDX
     ...MDXComponents,
     // Registra o componente globalmente
     LabStructure,
+    LabTable,
+    LabTeamMembers,
+    LabSubmit,
+    LabFromTemplate,
 };
