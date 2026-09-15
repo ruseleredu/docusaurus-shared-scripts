c82698d25b2ea9d842eeb51a5136beb0f34d0f18
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-09-14 23:47:32 -0300

    Update navbar.ts

--- navbar.ts
+++ navbar.ts
@@ -142,11 +142,11 @@ const navbarItems: NavbarItem[] = [
         ],
     },
     // { to: "/blog", label: "Blog", position: "right" },
-    { to: "/pdf/CT2026.pdf", label: "Calendário", position: "right", target: '_blank' },
+    //{ to: "/pdf/CT2026.pdf", label: "Calendário", position: "right", target: '_blank' },
     // https://ruseleredu.github.io/iiot-docs/espconnect/
     {
-        href: "https://ruseleredu.github.io/iiot-docs/espconnect/",
-        label: "ESPConnect",
+        href: "https://github.com/ruseleredu/iiot-docs/issues",
+        label: "Issues",
         position: "right",
     },
     {
